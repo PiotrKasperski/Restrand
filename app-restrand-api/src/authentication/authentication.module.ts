0bf97bccd1dcd654cea96483bf7950e24c4de302
Piotr Kasperski <Piotr.Kasperski94@gmail.com>
2022-03-20 21:28:01 +0100

    docker fix

--- app-restrand-api/src/authentication/authentication.module.ts
+++ app-restrand-api/src/authentication/authentication.module.ts
@@ -5,10 +5,12 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [AuthenticationService, LocalStrategy, JwtStrategy],
   imports: [
+    ConfigModule,
     PassportModule,
     JwtModule.register({
       secret: 'secret', //TODO: change to env variable
